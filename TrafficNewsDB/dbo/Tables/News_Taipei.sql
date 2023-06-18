6ca90dcac291405af1aa3585c7fddf0521eaa4c2
SaDoshi <dodo940816@gmail.com>
2023-06-18 17:44:20 +0800

    加入專案檔案。

---
+++ TrafficNewsDB/dbo/Tables/News_Taipei.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[News_Taipei] (
+    [ID]         DECIMAL (18)    IDENTITY (1, 1) NOT NULL,
+    [chtmessage] NVARCHAR (100)  NULL,
+    [engmessage] NVARCHAR (100)  NULL,
+    [starttime]  VARCHAR (14)    NULL,
+    [endtime]    VARCHAR (14)    NULL,
+    [updatetime] VARCHAR (14)    NULL,
+    [content]    NVARCHAR (1000) NULL,
+    [url]        NVARCHAR (100)  NULL
+);
+
